4bacb335cfabb116af2f3250d97a441bd3ba91bd
Tom <xnosx@hotmail.fr>
2024-09-06 11:33:51 +0200

    Fix multiple files formatting

--- src/Services/Response.php
+++ src/Services/Response.php
@@ -6,19 +6,15 @@ trait Response
 {
   public function render(string $view, array $data = ['section' => '', 'action' => ''])
   {
-    if (!empty($data))
-    {
-      foreach ($data as $key => $value)
-      {
+    if (!empty($data)) {
+      foreach ($data as $key => $value) {
         ${'view_' . $key} = $value;
       }
     }
-    if (!isset($section))
-    {
+    if (!isset($section)) {
       $section = '';
     }
-    if (!isset($action))
-    {
+    if (!isset($action)) {
       $action = '';
     }
     include_once __DIR__ . '/../Views/' . $view . ".php";
