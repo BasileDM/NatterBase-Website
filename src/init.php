a0b2450b29f697e03718c543ad09106c6947593b
Tom <xnosx@hotmail.fr>
2024-09-06 11:14:47 +0200

    Fix session start in init.php

--- src/init.php
+++ src/init.php
@@ -1,12 +1,11 @@
 <?php
 
+session_start();
+
 use src\Controllers\HomeController;
 use src\Router\Router;
 
 require_once __DIR__ . '/autoload.php';
-
-session_start();
-
 require_once __DIR__ . "/../config.local.php";
 
 // if (DB_INITIALIZED == FALSE)
@@ -14,8 +13,6 @@ require_once __DIR__ . "/../config.local.php";
 //   $db = new src\Models\Database();
 //   echo $db->dbInit();
 // }
-
-// require_once __DIR__ . "/router.php";
 
 $router = new Router();
 
