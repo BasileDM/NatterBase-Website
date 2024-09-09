5447d886b3bac92ee3afaa399fb11c24c696d2fd
Tom <xnosx@hotmail.fr>
2024-09-09 14:44:27 +0200

    Add click outside closing

--- public/assets/js/menu.js
+++ public/assets/js/menu.js
@@ -1,9 +1,32 @@
 export function toggleMenu() {
-  const toggleButton = document.getElementById('menu-toggle');
-  const sidebar = document.getElementById('sidebar');
+  const toggleButton = document.getElementById("menu-toggle");
+  const sidebar = document.getElementById("sidebar");
 
-  toggleButton.addEventListener('click', function () {
-    sidebar.classList.toggle('hidden');
-    sidebar.classList.toggle('animate-slideIn');
+  // Burger button toggle
+  toggleButton.addEventListener("click", function () {
+    if (sidebar.classList.contains("hidden")) {
+      sidebar.classList.remove("hidden");
+      sidebar.classList.remove("animate-slideOut");
+      sidebar.classList.add("animate-slideIn");
+    } else {
+      sidebar.classList.remove("animate-slideIn");
+      sidebar.classList.add("animate-slideOut");
+
+      setTimeout(() => {
+        sidebar.classList.add("hidden");
+      }, 450);
+    }
+  });
+
+  // Close sidebar when clicking outside of it
+  document.addEventListener("click", function (event) {
+    if (!sidebar.contains(event.target) && !toggleButton.contains(event.target)) {
+      sidebar.classList.remove("animate-slideIn");
+      sidebar.classList.add("animate-slideOut");
+
+      setTimeout(() => {
+        sidebar.classList.add("hidden");
+      }, 450);
+    }
   });
 }
