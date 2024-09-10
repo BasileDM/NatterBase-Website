c06dafce2a8d5502d043a272bdd11e854521db4d
Tom <xnosx@hotmail.fr>
2024-09-10 11:25:44 +0200

    Fix scripts

--- public/assets/js/menu.js
+++ public/assets/js/menu.js
@@ -1,7 +1,10 @@
 export function toggleMenu() {
   const toggleButton = document.getElementById('menu-toggle');
   const sidebar = document.getElementById('sidebar');
-
+  if (!toggleButton || !sidebar) {
+    console.error('Menu toggle or sidebar element not found');
+    return;
+  }
   // Burger button toggle
   toggleButton.addEventListener('click', function() {
     if (sidebar.classList.contains('hidden')) {
@@ -12,19 +15,16 @@ export function toggleMenu() {
     else {
       sidebar.classList.remove('animate-slideIn');
       sidebar.classList.add('animate-slideOut');
-
       setTimeout(() => {
         sidebar.classList.add('hidden');
       }, 450);
     }
   });
-
   // Close sidebar when clicking outside of it
   document.addEventListener('click', function(event) {
     if (!sidebar.contains(event.target) && !toggleButton.contains(event.target)) {
       sidebar.classList.remove('animate-slideIn');
       sidebar.classList.add('animate-slideOut');
-
       setTimeout(() => {
         sidebar.classList.add('hidden');
       }, 450);
