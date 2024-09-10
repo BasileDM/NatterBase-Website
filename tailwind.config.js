5cd1637678941e769eb1b8f5dcff35b691142f99
Tom <xnosx@hotmail.fr>
2024-09-10 16:57:39 +0200

    Add toast animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,10 +20,28 @@ export const theme = {
           transform: 'translateX(-100%)',
         },
       },
+      toastSlideIn: {
+        '0%': {
+          transform: 'translateX(100%)',
+        },
+        '100%': {
+          transform: 'translateX(0)',
+        },
+      },
+      toastSlideOut: {
+        '0%': {
+          transform: 'translateX(0)',
+        },
+        '100%': {
+          transform: 'translateX(150%)',
+        },
+      },
     },
     animation: {
       slideIn: 'slideIn 0.5s ease-in-out',
       slideOut: 'slideOut 0.5s ease-in-out',
+      toastSlideIn: 'toastSlideIn 0.5s ease-in-out',
+      toastSlideOut: 'toastSlideOut 0.5s ease-in-out',
     },
   },
 };
