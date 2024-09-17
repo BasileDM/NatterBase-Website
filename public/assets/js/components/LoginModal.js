56faf30ee43347a2696c1e7ae10aaaba16c75ea1
Tom <xnosx@hotmail.fr>
2024-09-17 11:20:51 +0200

    Add registration form to login modal

--- public/assets/js/components/LoginModal.js
+++ public/assets/js/components/LoginModal.js
@@ -3,17 +3,24 @@ import { Toast } from './Toast.js';
 export class LoginModal {
     constructor(modalId, triggerButtonIds) {
         this.modalElement = document.getElementById(modalId);
+        this.registerForm = document.getElementById('register-form');
+        this.loginForm = document.getElementById('login-form');
+        this.titleElement = document.getElementById('login-modal-title');
         this.closeButton = document.getElementById('login-modal-close-btn');
-        this.submitButton = document.getElementById('login-modal-submit-btn');
+        this.submitLoginFormBtn = document.getElementById('login-modal-submit-btn');
+        this.submitRegisterFormBtn = document.getElementById('register-modal-submit-btn');
+        this.registerButton = document.getElementById('modal-register-btn');
         this.bindEvents(triggerButtonIds);
     }
     bindEvents(triggerButtonIds) {
+        // Open buttons
         triggerButtonIds.forEach(buttonId => {
             const button = document.getElementById(buttonId);
             if (button) {
                 button.addEventListener('click', () => this.open());
             }
         });
+        // Close button
         if (this.closeButton) {
             this.closeButton.addEventListener('click', () => this.close());
         }
@@ -23,9 +30,15 @@ export class LoginModal {
                 this.close();
             }
         });
-        // Submit button
-        if (this.submitButton) {
-            this.submitButton.addEventListener('click', async (event) => {
+        // Display register form
+        this.registerButton.addEventListener('click', () => {
+            this.registerForm.classList.remove('hidden');
+            this.loginForm.classList.add('hidden');
+            this.titleElement.textContent = 'Register';
+        });
+        // Submit login form button
+        if (this.submitLoginFormBtn) {
+            this.submitLoginFormBtn.addEventListener('click', async (event) => {
                 event.preventDefault();
                 const formData = {
                     mail: document.getElementById('mail').value,
@@ -41,6 +54,29 @@ export class LoginModal {
                     this.close();
                     sessionStorage.setItem('showToast', 'You are now logged in!');
                     window.location.href = '/app';
+                }
+                catch {
+                    new Toast('error', 'Failed sending request. Try again later.');
+                }
+            });
+        }
+        // Submit registration form button
+        if (this.submitRegisterFormBtn) {
+            this.submitRegisterFormBtn.addEventListener('click', async (event) => {
+                event.preventDefault();
+                const formData = {
+                    mail: document.getElementById('mail').value,
+                    password: document.getElementById('password').value,
+                };
+                try {
+                    const response = await RequestHelper.post('/register', formData);
+                    const responseBody = await response.json();
+                    if (!response.ok) {
+                        new Toast('error', responseBody.message || 'An error occurred');
+                        return;
+                    }
+                    this.close();
+                    window.location.href = '/home?confirmRegistration=true';
                 }
                 catch {
                     new Toast('error', 'Failed sending request. Try again later.');
--- src/Typescript/Components/LoginModal.ts
+++ src/Typescript/Components/LoginModal.ts
@@ -3,17 +3,28 @@ import { Toast } from './Toast.js';
 
 export class LoginModal {
   private modalElement: HTMLDialogElement;
+  private registerForm: HTMLFormElement;
+  private loginForm: HTMLFormElement;
+  private titleElement: HTMLElement;
   private closeButton: HTMLElement;
-  private submitButton: HTMLElement;
+  private submitLoginFormBtn: HTMLElement;
+  private submitRegisterFormBtn: HTMLElement;
+  private registerButton: HTMLElement;
 
   constructor(modalId: string, triggerButtonIds: string[]) {
     this.modalElement = document.getElementById(modalId) as HTMLDialogElement;
+    this.registerForm = document.getElementById('register-form') as HTMLFormElement;
+    this.loginForm = document.getElementById('login-form') as HTMLFormElement;
+    this.titleElement = document.getElementById('login-modal-title') as HTMLElement;
     this.closeButton = document.getElementById('login-modal-close-btn') as HTMLElement;
-    this.submitButton = document.getElementById('login-modal-submit-btn') as HTMLElement;
+    this.submitLoginFormBtn = document.getElementById('login-modal-submit-btn') as HTMLElement;
+    this.submitRegisterFormBtn = document.getElementById('register-modal-submit-btn') as HTMLElement;
+    this.registerButton = document.getElementById('modal-register-btn') as HTMLElement;
     this.bindEvents(triggerButtonIds);
   }
 
   private bindEvents(triggerButtonIds: string[]): void {
+    // Open buttons
     triggerButtonIds.forEach(buttonId => {
       const button = document.getElementById(buttonId);
       if (button) {
@@ -21,6 +32,7 @@ export class LoginModal {
       }
     });
 
+    // Close button
     if (this.closeButton) {
       this.closeButton.addEventListener('click', () => this.close());
     }
@@ -32,9 +44,16 @@ export class LoginModal {
       }
     });
 
-    // Submit button
-    if (this.submitButton) {
-      this.submitButton.addEventListener('click', async (event) => {
+    // Display register form
+    this.registerButton.addEventListener('click', () => {
+      this.registerForm.classList.remove('hidden');
+      this.loginForm.classList.add('hidden');
+      this.titleElement.textContent = 'Register';
+    })
+
+    // Submit login form button
+    if (this.submitLoginFormBtn) {
+      this.submitLoginFormBtn.addEventListener('click', async (event) => {
         event.preventDefault();
         const formData = {
           mail: (document.getElementById('mail') as HTMLInputElement).value,
@@ -51,6 +70,30 @@ export class LoginModal {
           this.close();
           sessionStorage.setItem('showToast', 'You are now logged in!');
           window.location.href = '/app';
+        }
+        catch {
+          new Toast('error', 'Failed sending request. Try again later.');
+        }
+      });
+    }
+
+    // Submit registration form button
+    if (this.submitRegisterFormBtn) {
+      this.submitRegisterFormBtn.addEventListener('click', async (event) => {
+        event.preventDefault();
+        const formData = {
+          mail: (document.getElementById('mail') as HTMLInputElement).value,
+          password: (document.getElementById('password') as HTMLInputElement).value,
+        };
+        try {
+          const response = await RequestHelper.post('/register', formData);
+          const responseBody = await response.json();
+          if (!response.ok) {
+            new Toast('error', responseBody.message || 'An error occurred');
+            return;
+          }
+          this.close();
+          window.location.href = '/home?confirmRegistration=true';
         }
         catch {
           new Toast('error', 'Failed sending request. Try again later.');
