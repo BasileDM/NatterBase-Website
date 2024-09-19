2498d0ef60b1ffe2ab838c5db804ec405144a50d
Tom <xnosx@hotmail.fr>
2024-09-19 12:06:44 +0200

    Start adding basic control panel bar

--- src/Views/app.php
+++ src/Views/app.php
@@ -1,16 +1,19 @@
 <?php
 include __DIR__ . '/Includes/header.php';
 ?>
-
+<div id="control-panel" class="p-4 flex border-b border-gray-700">
+  <div class="mr-4 flex flex-col">
+    <label for="bot-profile-selector">Bot profile</label>
+    <select name="bot-profile" id="bot-profile-selector" value="default" class="text-black">
+      <option value="default">None...</option>
+    </select>
+  </div>
+  <div class="mr-4 btn btn-base">Run bot</div>
+</div>
 <section id="app-dashboard">
   <h2>Dashboard</h2>
   <p><?= "This is the dashboard: data = " . $view_data . ""; ?></p>
-  <p class="mb-44">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium repudiandae asperiores velit accusamus ad, expedita voluptas cumque deleniti. Ex amet accusantium dicta veritatis eum temporibus odio inventore cupiditate fugiat. Cumque.</p>
-  <p class="mb-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium repudiandae asperiores velit accusamus ad, expedita voluptas cumque deleniti. Ex amet accusantium dicta veritatis eum temporibus odio inventore cupiditate fugiat. Cumque.</p>
-  <p class="mb-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium repudiandae asperiores velit accusamus ad, expedita voluptas cumque deleniti. Ex amet accusantium dicta veritatis eum temporibus odio inventore cupiditate fugiat. Cumque.</p>
-  <p class="mb-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium repudiandae asperiores velit accusamus ad, expedita voluptas cumque deleniti. Ex amet accusantium dicta veritatis eum temporibus odio inventore cupiditate fugiat. Cumque.</p>
-  <p class="mb-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium repudiandae asperiores velit accusamus ad, expedita voluptas cumque deleniti. Ex amet accusantium dicta veritatis eum temporibus odio inventore cupiditate fugiat. Cumque.</p>
-  <p class="mb-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium repudiandae asperiores velit accusamus ad, expedita voluptas cumque deleniti. Ex amet accusantium dicta veritatis eum temporibus odio inventore cupiditate fugiat. Cumque.</p>
+  <p class="mb-96">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium repudiandae asperiores velit accusamus ad, expedita voluptas cumque deleniti. Ex amet accusantium dicta veritatis eum temporibus odio inventore cupiditate fugiat. Cumque.</p>
   <p class="mb-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium repudiandae asperiores velit accusamus ad, expedita voluptas cumque deleniti. Ex amet accusantium dicta veritatis eum temporibus odio inventore cupiditate fugiat. Cumque.</p>
   <p class="mb-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium repudiandae asperiores velit accusamus ad, expedita voluptas cumque deleniti. Ex amet accusantium dicta veritatis eum temporibus odio inventore cupiditate fugiat. Cumque.</p>
   <p class="mb-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium repudiandae asperiores velit accusamus ad, expedita voluptas cumque deleniti. Ex amet accusantium dicta veritatis eum temporibus odio inventore cupiditate fugiat. Cumque.</p>
