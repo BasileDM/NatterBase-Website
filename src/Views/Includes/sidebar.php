241fcddceed8cd89d23e0910e98d0d2440c34955
Tom <xnosx@hotmail.fr>
2024-09-19 09:24:15 +0200

    Fix sidebar flex vs hidden display

--- src/Views/Includes/sidebar.php
+++ src/Views/Includes/sidebar.php
@@ -1,15 +1,41 @@
-<div id="sidebar"
-  class="absolute sm:relative w-64 h-full sm:h-auto bg-gray-900/40 backdrop-blur-lg backdrop-filter border-r border-gray-700 text-white shadow z-10 hidden flex-col min-h-0">
-  <!-- Sidebar content -->
-  <ul class="space-y-4 p-4 flex-1 overflow-y-auto">
-    <li><a href="/" class="hover:text-gray-300">Home</a></li>
-    <li><a href="/features" class="hover:text-gray-300">Features</a></li>
-  </ul>
-  <div class="p-4">
-    <?php if (isset($_SESSION['isAuth'])): ?>
-      <a id="sidebar-app-button" class="btn btn-base" href="/app">Web app</a>
-    <?php else: ?>
-      <span id="sidebar-login-button" class="btn btn-base">Web app</span>
-    <?php endif ?>
-  </div>
+<div id="sidebar" class="absolute sm:relative w-64 h-full sm:h-auto bg-gray-900/40 backdrop-blur-lg backdrop-filter border-r border-gray-700 text-white shadow z-10 hidden">
+  <div class="flex flex-col h-full">
+    <!-- Sidebar content -->
+    <div class="flex-grow">
+      <!-- Dashboard navigation -->
+      <div class="p-4">
+        <h3>App</h3>
+        <ul class="flex-1 overflow-y-auto">
+          <li class="hover:text-gray-300 hover:cursor-pointer hover:bg-gray-600 p-2 rounded w-full">Dashboard</li>
+          <li class="hover:text-gray-300 hover:cursor-pointer hover:bg-gray-600 p-2 rounded w-full">Bot Features</li>
+          <li class="hover:text-gray-300 hover:cursor-pointer hover:bg-gray-600 p-2 rounded w-full">App settings</li>
+          <li class="hover:text-gray-300 hover:cursor-pointer hover:bg-gray-600 p-2 rounded w-full">Account</li>
+        </ul>
+      </div>
+    </div>
+    <!-- Site navigation -->
+    <div class="p-4">
+      <h3>Website navigation</h3>
+      <ul class="flex-1 overflow-y-auto">
+        <a href="/">
+          <li class="hover:text-gray-300 hover:cursor-pointer hover:bg-gray-600 p-2 rounded w-full">
+            Home
+          </li>
+        </a>
+        <a href="/features">
+          <li class="hover:text-gray-300 hover:cursor-pointer hover:bg-gray-600 p-2 rounded w-full">
+            Features
+          </li>
+        </a>
+      </ul>
+    </div>
+    <div class="p-4">
+      <?php if (isset($_SESSION['isAuth'])): ?>
+        <a id="sidebar-app-button" class="btn btn-base" href="/app">Web app</a>
+        <a id="sidebar-logout-button" class="btn btn-alert" href="/logout">Logout</a>
+      <?php else: ?>
+        <span id="sidebar-login-button" class="btn btn-base">Web app</span>
+      <?php endif ?>
+    </div>
+  </div class="flex flex-col h-full">
 </div>
