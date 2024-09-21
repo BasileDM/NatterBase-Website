e34d053ed4883ad6786bd0fc2480503735b7ef90
Tom <xnosx@hotmail.fr>
2024-09-21 21:23:53 +0200

    Create bot settings section form

--- src/Views/app.php
+++ src/Views/app.php
@@ -12,6 +12,25 @@ include __DIR__ . '/Includes/Components/controlPanel.php';
 <section id="app-bot-settings" class="hidden">
   <h2>Bot settings</h2>
   <p>Please select a bot.</p>
+  <form action="" class="space-y-4">
+    <p>Creation date: </p>
+    <div>
+      <label for="bot-name">Bot profile name</label>
+      <input class="input" type="text" name="name" id="bot-name">
+    </div>
+    <div>
+      <label for="bot-platform">Bot platform</label>
+      <input class="input" type="text" name="platform" id="bot-platform">
+    </div>
+    <div>
+      <label for="bot-cooldown">Bot cooldown</label>
+      <input class="input" type="number" name="cooldown" id="bot-cooldown">
+    </div>
+    <div class="flex gap-2">
+      <span class="btn btn-success">Save</span>
+      <span class="btn btn-alert">Delete</span>
+    </div>
+  </form>
 </section>
 
 <section id="app-bot-features" class="hidden">
