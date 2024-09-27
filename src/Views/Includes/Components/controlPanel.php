0c5dd7fec725c7ec98568ded7c752ede7f18266a
Tom <xnosx@hotmail.fr>
2024-09-27 16:36:46 +0200

    Add icons on buttons and nav items

--- src/Views/Includes/Components/controlPanel.php
+++ src/Views/Includes/Components/controlPanel.php
@@ -3,10 +3,15 @@ $botProfiles = $view_userData['botProfiles'];
 ?>
 <div id="control-panel" class="pb-4 flex border-b border-gray-700 items-center justify-between">
   <div id="profile-buttons">
-    <div class="flex flex-col items-center gap-2">
-      <div id="create-bot-profile-btn" class="text-center btn btn-base w-full">Create a bot</div>
-      <select id="bot-profiles-selector" class="text-black self-center w-full max-w-40 rounded text-center hover:cursor-pointer" name="bot-profile">
-        <option value="default" selected hidden>Select a bot</option>
+    <div class="flex flex-col items-center gap-2 min-w-32">
+      <div id="create-bot-profile-btn" class="text-center btn btn-success w-full flex">
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
+          <path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z" />
+        </svg>
+        New bot
+      </div>
+      <select id="bot-profiles-selector" class="py-1 text-black self-center w-full max-w-40 rounded-full text-center hover:cursor-pointer" name="bot-profile">
+        <option value="default" selected hidden>Select bot</option>
         <?php foreach ($botProfiles as $key => $botProfile): ?>
           <option
             value="<?= $botProfile['idBot'] ?>"
@@ -18,8 +23,18 @@ $botProfiles = $view_userData['botProfiles'];
     </div>
   </div>
   <div id="bot-buttons">
-    <div id="run-bot-btn" class="btn btn-success items-center justify-center hidden">Run bot</div>
-    <div id="run-bot-btn-disabled" class="btn btn-success btn-disabled items-center justify-center">No bot selected</div>
+    <div id="run-bot-btn" class="btn btn-success items-center justify-center hidden rounded-full flex">
+      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
+        <path d="M320-200v-560l440 280-440 280Zm80-280Zm0 134 210-134-210-134v268Z" />
+      </svg>
+      Run bot
+    </div>
+    <div id="run-bot-btn-disabled" class="btn btn-success btn-disabled items-center justify-center rounded-full flex">
+      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
+        <path d="M320-200v-560l440 280-440 280Zm80-280Zm0 134 210-134-210-134v268Z" />
+      </svg>
+      No bot selected
+    </div>
   </div>
 </div>
 
--- src/Views/Includes/sidebar.php
+++ src/Views/Includes/sidebar.php
@@ -5,34 +5,39 @@
       <div class="flex-grow">
         <!-- Dashboard navigation -->
         <div class="p-4">
-          <h3>App</h3>
+          <h3>
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
+              <path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z" />
+            </svg>
+            App
+          </h3>
           <ul class="flex-1 overflow-y-auto">
             <li id="dashboard-app-nav-button" data-section="app-dashboard" class="flex hover:text-gray-300 hover:cursor-pointer hover:bg-gray-600 p-2 rounded w-full">
-              <svg class="mr-2" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
+              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
                 <path d="M240-400h320v-80H240v80Zm0-120h480v-80H240v80Zm0-120h480v-80H240v80ZM80-80v-720q0-33 23.5-56.5T160-880h640q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H240L80-80Zm126-240h594v-480H160v525l46-45Zm-46 0v-480 480Z" />
               </svg>
               Chat
             </li>
             <li id="settings-app-nav-button" data-section="app-bot-settings" class="flex hover:text-gray-300 hover:cursor-pointer hover:bg-gray-600 p-2 rounded w-full">
-              <svg class="mr-2" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
+              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
                 <path d="m370-80-16-128q-13-5-24.5-12T307-235l-119 50L78-375l103-78q-1-7-1-13.5v-27q0-6.5 1-13.5L78-585l110-190 119 50q11-8 23-15t24-12l16-128h220l16 128q13 5 24.5 12t22.5 15l119-50 110 190-103 78q1 7 1 13.5v27q0 6.5-2 13.5l103 78-110 190-118-50q-11 8-23 15t-24 12L590-80H370Zm70-80h79l14-106q31-8 57.5-23.5T639-327l99 41 39-68-86-65q5-14 7-29.5t2-31.5q0-16-2-31.5t-7-29.5l86-65-39-68-99 42q-22-23-48.5-38.5T533-694l-13-106h-79l-14 106q-31 8-57.5 23.5T321-633l-99-41-39 68 86 64q-5 15-7 30t-2 32q0 16 2 31t7 30l-86 65 39 68 99-42q22 23 48.5 38.5T427-266l13 106Zm42-180q58 0 99-41t41-99q0-58-41-99t-99-41q-59 0-99.5 41T342-480q0 58 40.5 99t99.5 41Zm-2-140Z" />
               </svg>
               Bot settings
             </li>
             <li id="bot-features-app-nav-button" data-section="app-bot-features" class="flex hover:text-gray-300 hover:cursor-pointer hover:bg-gray-600 p-2 rounded w-full">
-              <svg class="mr-2" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
+              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
                 <path d="m422-232 207-248H469l29-227-185 267h139l-30 208ZM320-80l40-280H160l360-520h80l-40 320h240L400-80h-80Zm151-390Z" />
               </svg>
               Bot features
             </li>
             <li id="account-app-nav-button" data-section="app-account" class="flex hover:text-gray-300 hover:cursor-pointer hover:bg-gray-600 p-2 rounded w-full">
-              <svg class="mr-2" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
+              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
                 <path d="M400-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM80-160v-112q0-33 17-62t47-44q51-26 115-44t141-18h14q6 0 12 2-8 18-13.5 37.5T404-360h-4q-71 0-127.5 18T180-306q-9 5-14.5 14t-5.5 20v32h252q6 21 16 41.5t22 38.5H80Zm560 40-12-60q-12-5-22.5-10.5T584-204l-58 18-40-68 46-40q-2-14-2-26t2-26l-46-40 40-68 58 18q11-8 21.5-13.5T628-460l12-60h80l12 60q12 5 22.5 11t21.5 15l58-20 40 70-46 40q2 12 2 25t-2 25l46 40-40 68-58-18q-11 8-21.5 13.5T732-180l-12 60h-80Zm40-120q33 0 56.5-23.5T760-320q0-33-23.5-56.5T680-400q-33 0-56.5 23.5T600-320q0 33 23.5 56.5T680-240ZM400-560q33 0 56.5-23.5T480-640q0-33-23.5-56.5T400-720q-33 0-56.5 23.5T320-640q0 33 23.5 56.5T400-560Zm0-80Zm12 400Z" />
               </svg>
               Account
             </li>
             <li id="keys-app-nav-button" data-section="app-keys" class="flex hover:text-gray-300 hover:cursor-pointer hover:bg-gray-600 p-2 rounded w-full">
-              <svg class="mr-2" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
+              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
                 <path d="M280-400q-33 0-56.5-23.5T200-480q0-33 23.5-56.5T280-560q33 0 56.5 23.5T360-480q0 33-23.5 56.5T280-400Zm0 160q-100 0-170-70T40-480q0-100 70-170t170-70q67 0 121.5 33t86.5 87h352l120 120-180 180-80-60-80 60-85-60h-47q-32 54-86.5 87T280-240Zm0-80q56 0 98.5-34t56.5-86h125l58 41 82-61 71 55 75-75-40-40H435q-14-52-56.5-86T280-640q-66 0-113 47t-47 113q0 66 47 113t113 47Z" />
               </svg>
               Local Keys
@@ -43,24 +48,47 @@
     <?php endif ?>
     <!-- Site navigation -->
     <div id="website-mobile-nav" class="p-4">
-      <h3>Website navigation</h3>
+      <h3>
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
+          <path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z" />
+        </svg>
+        Website
+      </h3>
       <ul class="flex-1 overflow-y-auto">
         <a href="/">
-          <li class="hover:text-gray-300 hover:cursor-pointer hover:bg-gray-600 p-2 rounded w-full">
+          <li class="flex hover:text-gray-300 hover:cursor-pointer hover:bg-gray-600 p-2 rounded w-full">
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
+              <path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z" />
+            </svg>
             Home
           </li>
         </a>
         <a href="/features">
-          <li class="hover:text-gray-300 hover:cursor-pointer hover:bg-gray-600 p-2 rounded w-full">
-            Features
+          <li class="flex hover:text-gray-300 hover:cursor-pointer hover:bg-gray-600 p-2 rounded w-full">
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
+              <path d="M320-240h320v-80H320v80Zm0-160h320v-80H320v80ZM240-80q-33 0-56.5-23.5T160-160v-640q0-33 23.5-56.5T240-880h320l240 240v480q0 33-23.5 56.5T720-80H240Zm280-520v-200H240v640h480v-440H520ZM240-800v200-200 640-640Z" />
+            </svg>
+            Docs
           </li>
         </a>
       </ul>
     </div>
     <div class="p-4">
       <?php if (isset($_SESSION['isAuth'])): ?>
-        <a id="sidebar-app-button" class="btn btn-base" href="/app">Web app</a>
-        <a id="sidebar-logout-button" class="btn btn-alert" href="/logout">Logout</a>
+        <?php if ($view_section != 'app'): ?>
+        <a id="sidebar-app-button" class="btn btn-base flex" href="/app">
+          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
+            <path d="M480-80q-82 0-155-31.5t-127.5-86Q143-252 111.5-325T80-480q0-83 31.5-155.5t86-127Q252-817 325-848.5T480-880q83 0 155.5 31.5t127 86q54.5 54.5 86 127T880-480q0 82-31.5 155t-86 127.5q-54.5 54.5-127 86T480-80Zm0-82q26-36 45-75t31-83H404q12 44 31 83t45 75Zm-104-16q-18-33-31.5-68.5T322-320H204q29 50 72.5 87t99.5 55Zm208 0q56-18 99.5-55t72.5-87H638q-9 38-22.5 73.5T584-178ZM170-400h136q-3-20-4.5-39.5T300-480q0-21 1.5-40.5T306-560H170q-5 20-7.5 39.5T160-480q0 21 2.5 40.5T170-400Zm216 0h188q3-20 4.5-39.5T580-480q0-21-1.5-40.5T574-560H386q-3 20-4.5 39.5T380-480q0 21 1.5 40.5T386-400Zm268 0h136q5-20 7.5-39.5T800-480q0-21-2.5-40.5T790-560H654q3 20 4.5 39.5T660-480q0 21-1.5 40.5T654-400Zm-16-240h118q-29-50-72.5-87T584-782q18 33 31.5 68.5T638-640Zm-234 0h152q-12-44-31-83t-45-75q-26 36-45 75t-31 83Zm-200 0h118q9-38 22.5-73.5T376-782q-56 18-99.5 55T204-640Z" />
+          </svg>
+          Web app
+        </a>
+        <?php endif ?>
+        <a id="sidebar-logout-button" class="btn btn-alert flex" href="/logout">
+          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
+            <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z" />
+          </svg>
+          Logout
+        </a>
       <?php else: ?>
         <span id="sidebar-login-button" class="btn btn-base">Web app</span>
       <?php endif ?>
