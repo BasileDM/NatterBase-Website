992a32066935e42272b2c9eb3b34d5e964117a0e
Tom <xnosx@hotmail.fr>
2024-10-10 17:04:55 +0200

    Add PHPMailer dependancy and change sendmail method

--- src/Services/MailService.php
+++ src/Services/MailService.php
@@ -2,9 +2,14 @@
 
 namespace src\Services;
 
+require __DIR__ . '/../../vendor/autoload.php';
+
+use Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+
 final class MailService
 {
-  public static function sendActivationMail($user): bool
+  public static function sendActivationMail($user)
   {
     $name = $user->getUsername();
     $email = $user->getMail();
@@ -13,24 +18,43 @@ final class MailService
     $token = self::generateActivationToken($userId);
     $link = FULL_URL . "activate?token=" . urlencode($token);
 
-    $subject = "Natterbase Account Activation";
-    $message = "
-        <html>
-        <body>
-            <p>Hello $name,</p>
-            <p>Please activate your account using the link below:</p>
-            <p>Not working link yet</p>
-            <p>The Natterbase Team.</p>
-        </body>
-        </html>";
-    $message = wordwrap($message, 70, "\n\n");
+    $mail = new PHPMailer(true);  // Create a new PHPMailer instance
 
-    $headers = "From: Natterbase <contact@natterbase.com>\r\n";
-    $headers .= "Content-Type: text/html; charset=\"UTF-8\"\n";
-    $headers .= "Content-Transfer-Encoding: 8bit\n";
+    try {
+      // Gmail SMTP server settings
+      $mail->isSMTP();
+      $mail->Host       = SMTP_HOST;
+      $mail->SMTPAuth   = true;
+      $mail->Username   = SMTP_USERNAME;
+      $mail->Password   = SMTP_PASSWORD;
+      $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+      $mail->Port       = SMTP_PORT;
 
-    $sentMail = mail($email, $subject, $message, $headers);
-    return $sentMail;
+      // Recipients
+      $mail->setFrom(SMTP_FROM_EMAIL, SMTP_FROM_NAME);
+      $mail->addAddress($email, $name);
+
+      // Content
+      $mail->isHTML(true);
+      $mail->Subject = 'Natterbase Account Activation';
+      $mail->Body    = "
+            <html>
+            <body>
+                <p>Hello $name,</p>
+                <p>Please activate your account using the link below:</p>
+                <p><a href='$link'>Activate your account</a></p>
+                <p>The Natterbase Team.</p>
+            </body>
+            </html>";
+      $mail->AltBody = "Hello $name,\n\nPlease activate your account using this link: $link\n\nThe Natterbase Team.";  // Plain text alternative
+
+      // Send the email
+      $mail->send();
+      return true;
+    } catch (Exception $e) {
+      echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+      return false;
+    }
   }
 
   private static function generateActivationToken($userId): string
