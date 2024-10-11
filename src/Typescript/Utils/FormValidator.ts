78fa3a68a06d6aacde35812827bf808df4be7722
Tom <xnosx@hotmail.fr>
2024-10-11 12:38:34 +0200

    Change mail regex

--- src/Typescript/Utils/FormValidator.ts
+++ src/Typescript/Utils/FormValidator.ts
@@ -128,7 +128,7 @@ export class FormValidator {
   }
 
   private isValidEmail(email: string): boolean {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
     return emailRegex.test(email);
   }
 }
