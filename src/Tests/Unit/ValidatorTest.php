60e4671a79a0822046a11d2b1099f6e704e05e1d
Tom <xnosx@hotmail.fr>
2024-10-15 11:57:49 +0200

    Refactor validator testing array

--- src/Tests/Unit/ValidatorTest.php
+++ src/Tests/Unit/ValidatorTest.php
@@ -7,24 +7,22 @@ use src\Utils\Validator;
 
 class ValidatorTest extends TestCase
 {
+  private array $inputs = [
+    'mail' => 'a@a.com',
+    'username' => 'aaa',
+    'password' => 'aaaaaaaa',
+    'confirmPassword' => 'aaaaaaaa',
+    'gdpr' => true,
+    'name' => 'aaaa',
+    'twitchJoinChannel' => 'aaaa',
+    'openaiPrePrompt' => 'aaaa',
+    'cooldownTime' => '0',
+    'idBot' => '1',
+  ];
 
   public function testValidateInputsValid(): void
   {
-    $inputs = [
-      'mail' => 'a@a.com',
-      'username' => 'aaa',
-      'password' => 'aaaaaaaa',
-      'confirmPassword' => 'aaaaaaaa',
-      'gdpr' => true,
-      'name' => 'aaaa',
-      'twitchJoinChannel' => 'aaaa',
-      'openaiPrePrompt' => 'aaaa',
-      'cooldownTime' => '0',
-      'idBot' => '1',
-    ];
-
-    // Validate
-    $result = Validator::validateInputs($inputs);
+    $result = Validator::validateInputs($this->inputs);
     // Password hash will always be different so needs it's own testing
     $passwordHash = $result['sanitized']['passwordHash'];
     unset($result['sanitized']['passwordHash']);
@@ -48,21 +46,11 @@ class ValidatorTest extends TestCase
 
   public function testValidateInputsInvalidMail(): void
   {
-    $inputs = [
-      'mail' => 'a@a',
-      'username' => 'aaa',
-      'password' => 'aaaaaaaa',
-      'confirmPassword' => 'aaaaaaaa',
-      'gdpr' => true,
-      'name' => 'aaaa',
-      'twitchJoinChannel' => 'aaaa',
-      'openaiPrePrompt' => 'aaaa',
-      'cooldownTime' => '0',
-      'idBot' => '1',
-    ];
+    $wrongInputs = $this->inputs;
+    $wrongInputs['mail'] = 'wrongMail';
 
     // Validate
-    $result = Validator::validateInputs($inputs);
+    $result = Validator::validateInputs($wrongInputs);
 
     $this->assertArrayHasKey("errors", $result, "Errors key should exist");
     $this->assertArrayHasKey("mail", $result["errors"], "Mail key should exist in errors");
@@ -71,21 +59,11 @@ class ValidatorTest extends TestCase
 
   public function testValidateInputsMismatchPassword(): void
   {
-    $inputs = [
-      'mail' => 'a@a.com',
-      'username' => 'aaa',
-      'password' => 'aaaaaaaa',
-      'confirmPassword' => 'bbbbbbbb',
-      'gdpr' => true,
-      'name' => 'aaaa',
-      'twitchJoinChannel' => 'aaaa',
-      'openaiPrePrompt' => 'aaaa',
-      'cooldownTime' => '0',
-      'idBot' => '1',
-    ];
+    $wrongInputs = $this->inputs;
+    $wrongInputs['confirmPassword'] = 'differentPw';
 
     // Validate
-    $result = Validator::validateInputs($inputs);
+    $result = Validator::validateInputs($wrongInputs);
 
     $this->assertArrayHasKey("errors", $result, "Errors key should exist");
     $this->assertArrayHasKey("password", $result["errors"], "Password key should exist in errors");
